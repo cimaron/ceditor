dce72e870db8e69013695445277eadb5980a3234
Cimaron Shanahan <cshanahan@cimaron.net>
2016-02-22 21:26:28 -0800

    Improve active window handling

--- assets/js/window.js
+++ assets/js/window.js
@@ -35,20 +35,16 @@
 		this.element.draggable({ handle: this.titlebar });
 		this.element.resizable();
 		
-		this.element.on('mousedown', function() {
-			this.element.css('z-index', CEWindow.topZ);
-			CEWindow.topZ++;
-		}.bind(this));
-
-		this.element.css('z-index', CEWindow.topZ);
-		CEWindow.topZ++;
-
 		this.closeBtn.on('click', this.close.bind(this));
+
+		this.element.on('mousedown', this.setActive.bind(this));
+		this.setActive();
 
 		CEWidget.prototype.init.apply(this, []);
 	};
 
 	CEWindow.topZ = 2;
+	CEWindow.activeWindow = null;
 
 	CEWindow.prototype.setTitle = function(title) {
 		this.titlebar.find('.ce-window-title-text').text(title);
@@ -56,6 +52,29 @@
 
 	CEWindow.prototype.close = function() {
 		this.element.remove();
+	};
+
+	CEWindow.prototype.isActive = function() {
+		return this.active;
+	};
+
+	/**
+	 * Set current window as active
+	 */
+	CEWindow.prototype.setActive = function() {
+	
+		if (this.active) {
+			return;
+		}
+
+		if (CEWindow.activeWindow) {
+			CEWindow.activeWindow.active = false;
+		}
+
+		CEWindow.activeWindow = this;
+		this.active = true;
+		this.element.css('z-index', CEWindow.topZ);
+		CEWindow.topZ++;
 	};
 
 	window.CEWindow = CEWindow;
